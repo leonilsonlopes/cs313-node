3102122e7d5f2acac14de4696553931aadbe40c0
Leonilson <leonilson.lopes1@gmail.com>
2018-06-29 00:40:57 +0100

    week9 ponder

--- controllers/week09/week09.js
+++ controllers/week09/week09.js
@@ -1,10 +1,10 @@
 var express = require('express')
   , router = express.Router()
 
-console.lot('#### file week09.js');
+console.log('#### file week09.js');
   
 router.all('/week09_ponder', (req, res) => {  
-	console.lot('#### router week09_ponder');
+	console.log('#### router week09_ponder');
 	res.render('week09_ponder!!!');
   //res.render('./views/week09_ponder/postalCalculator');
 });
