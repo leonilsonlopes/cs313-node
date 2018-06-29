3e735f23cf3e0dfe9fc073ec854d9c950e4f3c02
Leonilson <leonilson.lopes1@gmail.com>
2018-06-29 02:50:10 +0100

    week9 ponder

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ var express = require('express')
 router.use('/week09', require('./week09/week09'));
 
 router.all('/', function(req, res) {
-  res.render('home/index')
+  res.sendFile('home/index.html')
 })
 
 router.all('/about', function(req, res) {
