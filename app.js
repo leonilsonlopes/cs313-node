87733d18f04e27ad621f5f13285b3b01ee08ac7d
Leonilson <leonilson.lopes1@gmail.com>
2018-06-29 02:37:37 +0100

    week9 ponder

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
 app = express();
 
 
-app.set('views', __dirname + '/view');
+app.set('views', __dirname + '/view/');
 
 //app.use(express.static(__dirname + '/week09_ponder'));
 app.set('view engine', 'ejs');
