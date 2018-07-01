174a688ec06edcc982c3b7c82b1e89ddf53259d9
Leonilson <leonilson.lopes1@gmail.com>
2018-07-01 21:06:01 +0100

    week9 ponder

--- views/project02/js/ajaxCalls.js
+++ views/project02/js/ajaxCalls.js
@@ -1,10 +1,35 @@
-var ajax = require("node.ajax");
-
-function getListOfCurrencies(){
-	console.log("### called function: getListOfCurrencies");
-	var res = yield ajax("https://peaceful-lowlands-49839.herokuapp.com/project02/getListOfCurrencies","GET",{
-    params: value
-	},{'Content-Type': 'application/x-www-form-urlencoded'},"utf8");
+$(document).ready(function() {
 	
-	return res;
+	var table = $('#currencies').DataTable();
+
+	$('#currencies').on( 'click', 'tbody tr', function () {
+		if ( $(this).hasClass('selected') ) {
+			$(this).removeClass('selected');
+		}
+		else {
+			table.$('tr.selected').removeClass('selected');
+			$(this).addClass('selected');
+		}
+		
+		$('input[name=code]').val(table.row( this ).data()[0]);
+		$('input[name=name]').val(table.row( this ).data()[1]);
+
+	} );
+
+
+} );
+
+$(document).ready(function(){	
+	buildCurrencyTable();
+});	
+
+	
+function buildCurrencyTable(){
+	$.get("https://peaceful-lowlands-49839.herokuapp.com/project02/getListOfCurrencies", function(data, status){
+		var t = $('#currencies').DataTable();
+		$.each(data, function (i, item) {
+			t.row.add([data[i].code, data[i].name]).draw(false);
+		});
+    });
+
 }
