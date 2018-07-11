eb1db2a5ae32079d6a46f73e37bfb74c38c792b1
Leonilson <leonilson.lopes1@gmail.com>
2018-07-11 22:47:20 +0100

    week9 ponder

--- views/project02/js/tradingCalls.js
+++ views/project02/js/tradingCalls.js
@@ -1,5 +1,26 @@
 var SERVICE = "https://peaceful-lowlands-49839.herokuapp.com/project02/";
 
+function buildBuyDropDown(){
+	
+	
+	$.get(SERVICE + "getListOfCurrencies", function(data, status){
+				
+
+		var t = $('#buyDropDownList');
+		$.each(data, function (i, item) {
+			
+			var code = data[i].code;
+			var name = data[i].name;
+			
+			t.append("<div class='dropdown-divider'></div><a class='dropdown-item'>" . code . ' - ' . name .'</a>';";
+			
+
+		});
+
+		
+    });
+
+}
 
 
 
