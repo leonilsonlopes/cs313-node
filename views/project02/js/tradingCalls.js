62a8eed522f54cf76040c3f675d37b28723dc840
Leonilson <leonilson.lopes1@gmail.com>
2018-07-12 00:38:01 +0100

    week9 ponder

--- views/project02/js/tradingCalls.js
+++ views/project02/js/tradingCalls.js
@@ -9,7 +9,7 @@ function buildBuyDropDown(){
 			var code = data[i].code;
 			var name = data[i].name;
 
-			$('#buyDropDownList').append("<div class='dropdown-divider'></div><option class='dropdown-item' onclick='buildBuySelectedCoin(\"" + getElementById('buyDropDownList').value + "\")'>" + code + ' - ' + name + '</option>');
+			$('#buyDropDownList').append("<div class='dropdown-divider'></div><option class='dropdown-item' onclick='buildBuySelectedCoin(\"" + document.getElementById('buyDropDownList').value + "\")'>" + code + ' - ' + name + '</option>');
 			
 
 		});
