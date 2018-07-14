2c293c88c656edc162d1eda8853301e3ecf5889b
Leonilson <leonilson.lopes1@gmail.com>
2018-07-14 03:42:02 +0100

    week9 ponder

--- views/project02/js/tradingCalls.js
+++ views/project02/js/tradingCalls.js
@@ -28,7 +28,7 @@ function buildBuySelectedCoin(selected){
 		var code = selected[0];
 		var name = selected[1];
 		
-		$('#btnBuyCoin').val("Buy " + code);
+		document.getElementById('btnBuyCoin').value = "Buy " + code;
 
 		var t = $('#tableSelectedBuyCoin').DataTable();
 		t.clear();
@@ -42,7 +42,7 @@ function buildBuySelectedCoin(selected){
 						data.percent_change_1h + "%",
 						data.percent_change_24h + "%",
 						data.percent_change_7d + "%",
-						(d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear() + " - "; + d.getHours() + ':' + d.getMinutes().substr(-2) + ':' + d.getSeconds().substr(-2)
+						(d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear() + " - " + d.getHours() + ':' + d.getMinutes().substr(-2) + ':' + d.getSeconds().substr(-2)
 					
 					]).draw(false);
 					
